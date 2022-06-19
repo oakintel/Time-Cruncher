532ce483e1812efcdb490404f6745c5a4997139f
Soore Akintelure <103961262+oakintel@users.noreply.github.com>
2022-06-19 01:37:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+print('Hello World')
+
+
+
+
+
+# water = ("3:00 PM, 3:10")
+# division = water.split(',')
+# print(division[0:])
+
+
+
+
+#    return new_time
+
+
+
+
+def add_time(start, duration):
+
+    current_time = start.partition(':')[0]
+    meridian = start.partition(':')[2]
+  
+    print(current_time)
+    print(meridian)
+
+  days_of_the_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+
+
+print(add_time("3:00 PM", "3:10"))
